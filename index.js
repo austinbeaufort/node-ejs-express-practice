e41cd6ae2efb1684e7f02499b8c5427f2afc71e8
Austin Beaufort <tinyhatcoding@gmail.com>
2019-06-03 10:29:03 -0400

    post requests

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
@@ -10,8 +13,15 @@ app.get('/', (req, res) => {
 })
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact', {
+        qs: req.query
+    });
 });
+
+app.post('/contact', urlencodedParser, (req, res) => {
+    console.log(req.body);
+    res.render('contact-success', {data: req.body});
+})
 
 app.get('/profile/:name', (req, res) => {
     const data = {
